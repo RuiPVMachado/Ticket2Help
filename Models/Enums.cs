154605ddf01bac03459bcdeaefc339a4ac2ac8bd
Rui Machado <74636066+RuiPVMachado@users.noreply.github.com>
2025-05-22 16:23:31 +0100

    Criação do esqueleto + Models/DB
    Criação do esqueleto do projeto + ficheiros de Models e criação de DB

---
+++ Models/Enums.cs
@@ -0,0 +1,34 @@
+﻿namespace MODELS
+{
+    public enum TipoUtilizador
+    {
+        Tecnico,
+        Colaborador
+    }
+
+    public enum Prioridade
+    {
+        Alta,
+        Media,
+        Baixa
+    }
+
+    public enum TipoTicket
+    {
+        Hardware,
+        Software
+    }
+
+    public enum EstadoTicket
+    {
+        PorAtender,
+        EmAtendimento,
+        Atendido
+    }
+
+    public enum SubEstadoAtendimento
+    {
+        Resolvido,
+        NaoResolvido
+    }
+}
